b6ec5f8b4793edef4f9ab33baa484a01e3278a73
BEnser16 <ryandai1690@gmail.com>
2023-08-30 05:23:30 +0800

    del import

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import HomeNavbar from '../components/nav_homepage'
 import HomeCarousel from '../components/carousel_homepage'
-import Container from 'react-bootstrap/esm/Container'
 
 const HomePage = () => {
   return (
     <div>
-        <HomeNavbar/>
-        
-          <HomeCarousel />
-        
-       
-        
+        <HomeNavbar/>      
+          <HomeCarousel />     
         
     </div>
   )
